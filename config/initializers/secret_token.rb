4cb19c50b1801f106b98b07c940696711fed5a5e
Christian Moniatis <christian.moniatis@gmail.com>
2012-06-01 17:13:14 +0200

    Erster Versuch eines Blogs auf Basis von Ruby on Rails (3)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBlog::Application.config.secret_token = '8d7d4db9bc54e06955157034a1813a8474f1bda952da9f99da054c47524cef85aafa426293cedf03bba841d51f2cbd2937527b24a5577a9a0118f635b739b461'
